1cc9ae50b053d098a42ee341ab2d0ff8cad56b29
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-01-15 08:40:38 +1000

    fix crs usage

--- R/summarise-model.R
+++ R/summarise-model.R
@@ -2,13 +2,13 @@ aggregate_kriging_model_to_ASGS <- function(kriged_spdf, ASGS_level, ASGS_year) 
   SA_id <- glue::glue("{ASGS_level}_CODE{ASGS_year}")
 
   kriged_sf <- st_as_sf(kriged_spdf)
-  kriged_sf <- st_set_crs(kriged_sf, 4283)
-  kriged_sf <- st_transform(kriged_sf, crs = 4283)
+  kriged_sf <- st_set_crs(kriged_sf, crs_for_analyses$crs_num)
+  kriged_sf <- st_transform(kriged_sf, crs = crs_for_analyses$crs_num)
 
   ASGS_poly <- strayr::read_absmap(area = ASGS_level, year = ASGS_year) |>
     (\(x) x[!st_is_empty(x), ])() |>
     filter(!!rlang::sym(glue::glue("state_name_{ASGS_year}")) == "Queensland") |>
-    st_transform(crs = 4283) |>
+    st_transform(crs = crs_for_analyses$crs_num) |>
     select(!!SA_id := 1)
 
   qld_SAs_with_int_times <- st_join(ASGS_poly, kriged_sf)
