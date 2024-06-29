0e04bbdec334107ce9af5556708318e2baa07eaf
Rex Parsons <47549264+RWParsons@users.noreply.github.com>
2024-06-29 14:10:51 +1000

    add distance legend to static maps

--- R/vis-plotting-utils.R
+++ R/vis-plotting-utils.R
@@ -12,7 +12,7 @@ get_plotting_utils <- function() {
   )
 
   out_dpi <- 600
-
+  
   add_common_plot_features <- function(gg,
                                        theme = map_theme,
                                        x_limits = c(135, 158),
@@ -34,7 +34,48 @@ get_plotting_utils <- function() {
     if (add_dots_for_cities) {
       gg <- gg + geom_point(data = cities_data, aes(x = x, y = y))
     }
-    gg
+    
+    scale_legend <- list(
+      y_bar = -29.4,
+      y_text = -30,
+      x_min = 147.69529500846323,
+      x_max = 152.85606381166014,
+      label_text = c("0", "500km")
+    )
+    
+    d_rect <- bind_rows(
+      c(#name = "full bar", 
+        ymin = scale_legend$y_bar-0.1, 
+        ymax = scale_legend$y_bar,
+        xmin = scale_legend$x_min, 
+        xmax = scale_legend$x_max),
+      
+      c(#name = "tick_min", 
+        ymin = scale_legend$y_bar-0.2, 
+        ymax = scale_legend$y_bar+0.1,
+        xmin = scale_legend$x_min, 
+        xmax = scale_legend$x_min-0.1),
+      c(#name = "tick_min", 
+        ymin = scale_legend$y_bar-0.2, 
+        ymax = scale_legend$y_bar+0.1,
+        xmin = scale_legend$x_max, 
+        xmax = scale_legend$x_max-0.1)
+      
+    )
+    
+    gg + 
+      geom_rect(
+        data = d_rect, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)
+      ) +
+      geom_text(
+        data = tibble(
+          x = c(scale_legend$x_min, scale_legend$x_max),
+          y = scale_legend$y_text,
+          text = scale_legend$label_text
+        ),
+        aes(x = x, y = y, label = text), size = text_size
+      )
+    
   }
 
   other_cities <- data.frame(
